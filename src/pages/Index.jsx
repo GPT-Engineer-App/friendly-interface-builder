3f18ee2cc7cd4ecc81a929f6a0ce2d432789cc4f
Niels Bosma <niels@bosmainteractive.se>
2024-07-28 17:19:38 +0200

    Foo

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -78,7 +78,7 @@ const Index = () => {
   return (
     <div className="container mx-auto p-6 space-y-6">
 
-      <div className="bg-white p-6 rounded-lg shadow relative">
+      <div className="relative">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <TooltipProvider>
             <Tooltip>
